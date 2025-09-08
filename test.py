d213ef499bc57ac4d21c71a65f3ae08b9815dc9a
THXHE Programador <107508243+thzinprogramador@users.noreply.github.com>
2025-09-07 23:53:25 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-t
+import streamlit as st
+import firebase_admin
+import requests
+import datetime 
+import random
+import time
+from firebase_admin import credentials, db
+from io import BytesIO
+from PIL import Image
+
+
+# ==============================
+# CONFIGURAÇÃO DA PÁGINA
+# ==============================
+st.set_page_config(
+    page_title="Wave 2.0",
+    page_icon="🌊",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
