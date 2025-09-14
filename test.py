a47c5e75b489912c767bae0d02bbe184fe616cb2
THXHE Programador <107508243+thzinprogramador@users.noreply.github.com>
2025-09-13 22:53:04 -0300

    Update test.py

--- test.py
+++ test.py
@@ -607,7 +607,7 @@ def show_add_music_page():
                 st.success("✅ Acesso concedido!")
             else:
                 st.error("❌ Senha incorreta!")
-        return
+        st.stop()
     
     if st.session_state.show_add_form:
         with st.form("add_music_form", clear_on_submit=True):
